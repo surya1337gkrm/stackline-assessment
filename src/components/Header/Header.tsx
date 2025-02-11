266a8c60699ff101e4fbd70b921043e05153a2af
surya1337gkrm <suryavenkatesh0@gmail.com>
2025-02-11 13:20:24 -0800

    Base setup | React with TS, Tailwind CSS and Header Component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+const Header: React.FC = () => {
+  return (
+    <header className='bg-blue-950 shadow-md p-4 flex items-center justify-between'>
+      <div className='flex items-center space-x-4'>
+        <img src={logo} alt='Stackline logo' className='h-8 w-min' />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
